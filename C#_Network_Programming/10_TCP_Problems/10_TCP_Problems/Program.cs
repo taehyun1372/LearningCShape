8a2d22851dcb18461f15f09e0efe8e16a54e6a83
roy1117 <85148810+roy1117@users.noreply.github.com>
2025-04-10 18:30:04 +0900

    Added TCP Problems

---
+++ C#_Network_Programming/10_TCP_Problems/10_TCP_Problems/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace _10_TCP_Problems
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1501);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(ep);
+            server.Listen(5);
+            Socket clientConnection = server.Accept();
+            Console.WriteLine("A connection made");
+            while (true)
+            {
+                byte[] data = new byte[10];
+                var recv = clientConnection.Receive(data);
+                var stringData = Encoding.ASCII.GetString(data, 0, recv);
+                Console.WriteLine($"The received data  is {stringData}");
+                Console.WriteLine($"The length of the message  is {stringData.Length}");
+            }
+
+        }
+    }
+}
