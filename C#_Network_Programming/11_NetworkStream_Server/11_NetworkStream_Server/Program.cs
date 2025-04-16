23999bdba64f1ed265ff6bb1a0fb25f2f9d836a6
roy1117 <85148810+roy1117@users.noreply.github.com>
2025-04-16 12:04:47 +0900

    Added SteamNetwork Server/Client

---
+++ C#_Network_Programming/11_NetworkStream_Server/11_NetworkStream_Server/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.IO;
+
+namespace _11_NetworkStream_Server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1500);
+            Socket server = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            server.Bind(ep);
+            server.Listen(5);
+
+            Socket client = server.Accept();
+            Console.WriteLine("A connection detected");
+            NetworkStream ns = new NetworkStream(client);
+            StreamReader reader = new StreamReader(ns);
+            byte[] data = new byte[1024];
+            while(true)
+            {
+
+                //var recv = ns.Read(data, 0, data.Length);
+                //var message = Encoding.ASCII.GetString(data, 0, recv);
+
+                var message = reader.ReadLine();
+                Console.WriteLine($"The message is {message}");
+                Thread.Sleep(500);
+            }
+        }
+    }
+}
---
+++ C#_Network_Programming/12_NetworkStream_Client/12_NetworkStream_Client/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.IO;
+
+namespace _12_NetworkStream_Client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 1500);
+            Socket client = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            client.Connect(ep);
+
+            Console.WriteLine("A connection detected");
+            NetworkStream ns = new NetworkStream(client);
+            StreamWriter writer = new StreamWriter(ns);
+            writer.AutoFlush = true;
+            byte[] data = new byte[1024];
+            while(true)
+            {
+                if(ns.CanWrite)
+                {
+                    Console.WriteLine("Enter a message to send..");
+                    var message = Console.ReadLine();
+                    data = Encoding.ASCII.GetBytes(message);
+                    writer.WriteLine(message);
+                    //writer.Flush();
+                    //ns.Write(data);
+                }
+            }
+        }
+    }
+}
