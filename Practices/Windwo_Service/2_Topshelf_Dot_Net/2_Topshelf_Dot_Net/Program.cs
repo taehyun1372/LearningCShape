61f142c3df1906714e07168c80c56d1c47a85314
roy1117 <85148810+roy1117@users.noreply.github.com>
2025-05-15 16:07:50 +0900

    [2_Topshelf_Dot_Net] Added a Topshelf practice solution

---
+++ Practices/Windwo_Service/2_Topshelf_Dot_Net/2_Topshelf_Dot_Net/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Topshelf;
+
+namespace _2_Topshelf_Dot_Net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            System.IO.File.AppendAllText("C:\\install_log.txt",
+            $"Arguments: {string.Join(" ", args)} - {DateTime.Now}\n");
+
+            HostFactory.Run(x =>
+            {
+                x.Service<MyService>(s =>
+                {
+                    s.ConstructUsing(name => new MyService());
+                    s.WhenStarted(tc => tc.Start());
+                    s.WhenStopped(tc => tc.Stop());
+                });
+
+                x.RunAsLocalSystem();
+                x.SetServiceName("MyService");
+                x.SetDisplayName("My Sample Service");
+                x.SetDescription("This is a Topshelf-powered service");
+            });
+
+        }
+    }
+
+    public class MyService
+    {
+        public bool Start()
+        {
+            Console.WriteLine("Service started");
+            return true;
+        }
+
+        public bool Stop()
+        {
+            Console.WriteLine("Service stopped");
+            return true;
+        }
+    }
+}
