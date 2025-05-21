f8dcee0eb77bb63d660a71a4b039ee7410bd18a2
roy1117 <85148810+roy1117@users.noreply.github.com>
2025-05-21 18:43:24 +0900

    [WCF_Server1] [WCF_Client1]

---
+++ Practices/5_WCF_Server1/5_WCF_Server1/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace _5_WCF_Server1
+{
+    [ServiceContract]
+    public interface IHelloService
+    {
+        [OperationContract]
+        string SayHello(string name);
+    }
+
+    public class HelloService : IHelloService
+    {
+        public string SayHello(string name)
+        {
+            return $"Hello, {name} !";
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var baseAddress = new Uri("http://localhost:8000/HelloService");
+            using (var host = new ServiceHost(typeof(HelloService), baseAddress))
+            {
+                host.AddServiceEndpoint(typeof(IHelloService), new BasicHttpBinding(), "");
+                host.Open();
+                Console.WriteLine("Service is running..Press Enter to exit");
+                Console.ReadLine();
+                host.Close();
+            }
+
+        }
+    }
+}
---
+++ Practices/6_WCF_Client1/6_WCF_Client1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+
+namespace _6_WCF_Client1
+{
+    [ServiceContract]
+    public interface IHelloService
+    {
+        [OperationContract]
+        string SayHello(string name);
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var address = new EndpointAddress("http://localhost:8000/HelloService");
+            var binding = new BasicHttpBinding();
+
+            var factory = new ChannelFactory<IHelloService>(binding, address);
+            var proxy = factory.CreateChannel();
+
+            string result = proxy.SayHello("Alice");
+            Console.WriteLine(result);
+
+            Console.WriteLine("Client is running..Press Enter to exit");
+            Console.ReadLine();
+            ((IClientChannel)proxy).Close();
+            factory.Close();
+        }
+    }
+}
