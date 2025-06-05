355490a714ac80ff0938f3328ec5ce21eb0c6f54
Roy Choi <roy.choi@edwardsvacuum.com>
2025-06-05 17:21:06 +0900

    [13_Styles] [14_Routed_Events]

---
+++ Practices/14_Routed_Events/14_Routed_Events/CustomTextBox.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace _14_Routed_Events
+{
+    public class CustomTextBox : TextBox
+    {
+        public static RoutedEvent TwentyDetectedEvent = EventManager.RegisterRoutedEvent("TwentyDetected",
+                RoutingStrategy.Bubble, typeof(RoutedEventHandler), typeof(CustomTextBox));
+        public CustomTextBox() : base()
+        {
+        }
+
+        // 2️⃣ Provide CLR wrapper
+        public event RoutedEventHandler TwentyDetected
+        {
+            add { AddHandler(TwentyDetectedEvent, value); }
+            remove { RemoveHandler(TwentyDetectedEvent, value); }
+        }
+
+        protected override void OnTextChanged(TextChangedEventArgs e)
+        {
+            base.OnTextChanged(e);
+            
+            if ( Text == "20")
+            {
+                RaiseEvent(new RoutedEventArgs(TwentyDetectedEvent));
+            }
+        }
+
+    }
+}
---
+++ Practices/14_Routed_Events/14_Routed_Events/MainWindow.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _14_Routed_Events
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            MyPanel.AddHandler( //We can catch any routed event that is not listed in XMAL 
+                UIElement.MouseDownEvent,
+                new MouseButtonEventHandler(StackPanel_Click),
+                handledEventsToo: true
+                );
+        }
+
+        private void StackPanel_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("An button click event from a child element has been catched");
+            if (e.OriginalSource is Button btn)
+            {
+                MessageBox.Show(btn.Content.ToString()); //sendr : who is handlling the event, e.OriginalSource : who triggered the event 
+            }
+        }
+
+        private void MyPanel_TwentyDetected(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("An value 20 event from a child element has been catched");
+            if (e.OriginalSource is CustomTextBox tb)
+            {
+                MessageBox.Show(tb.Text); //sendr : who is handlling the event, e.OriginalSource : who triggered the event 
+            }
+        }
+    }
+}
