8e9d8dc97b88c062764c823f2b7e61de1e8decd0
Roy Choi <roy.choi@edwardsvacuum.com>
2025-06-24 16:33:54 +0900

    [21_Maskify]

---
+++ Practices/20_Codesys_Automation_Interface/20_Codesys_Automation_Interface/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CoDeSys;
+
+namespace _20_Codesys_Automation_Interface
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var codesys = new CCoDeSys();
+            codesys.OpenProject(@"D:\git\Tumalo\Modbus_Test_Project.project");
+        }
+    }
+}
---
+++ Practices/21_Maskify/21_Maskify/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _21_Maskify
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(maskify2("12"));
+            
+            string message = "Hello World!";
+            Console.WriteLine(message.Substring(message.Length - 4));
+            Console.WriteLine(message.Substring(message.Length - 4).PadLeft(message.Length, '#'));
+
+            Console.ReadLine();
+        }
+
+        static string maskify(string input)
+        {
+            string result = "";
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c;
+                if (i < input.Length - 4)
+                {
+                    c = '#';
+                }
+                else
+                {
+                    c = input[i];
+                }
+                result += c;
+            }
+            return result;
+        }
+
+        static string maskify2(string cc)
+        {
+            return cc.Length > 4 ? cc.Substring(cc.Length - 4).PadLeft(cc.Length, '#') : cc;
+        }
+
+
+    }
+}
