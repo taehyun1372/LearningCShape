c5a4d99740d2839369835bcf566d1c1222dc2a8a
taehyun1372 <83771699+taehyun1372@users.noreply.github.com>
2026-01-11 21:35:35 +0900

    Add Open CV Project

---
+++ OpenCV/1_Intro/1_Intro/1_Intro/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing.Printing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenCvSharp;
+//using System.Drawing;
+
+namespace _1_Intro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Cv2.GetVersionString());
+            OpenCvSharp.Size cv_size = new OpenCvSharp.Size();
+            //System.Drawing.Size draw_size = new System.Drawing.Size();
+
+            //Mat color = new Mat(new Size(10, 10), MatType.CV_8U);
+            //Mat gray = new Mat(10, 10, MatType.CV_8U);
+            Mat src = Cv2.ImRead("Ref\\image.jpg");
+            Mat gray = new Mat();
+            Mat hist = new Mat();
+            Mat result = Mat.Ones(new Size(256, src.Height), MatType.CV_8UC1);
+            Mat dst = new Mat();
+
+            Cv2.CvtColor(src, gray, ColorConversionCodes.BGR2GRAY);
+            Cv2.CalcHist(new Mat[] { gray }, new int[] { 0 }, null, hist, 1, new int[] { 256 }, new Rangef[] { new Rangef(0, 256) });
+            Cv2.Normalize(hist, hist, 0, 255, NormTypes.MinMax);
+
+            for (int i = 0; i < hist.Rows; i++)
+            {
+                Cv2.Line(result, new Point(i, src.Height), new Point(i, src.Height - hist.Get<float>(i)), Scalar.White);
+            }
+            Cv2.HConcat(new Mat[] { gray, result }, dst);
+            //Cv2.ImShow("dst", dst);
+            Cv2.WaitKey(0);
+            Cv2.DestroyAllWindows();
+
+            Scalar s1 = new Scalar(255, 127);
+            Scalar s2 = Scalar.Yellow;
+            Scalar s3 = Scalar.All(99);
+
+            Console.WriteLine(s1);
+            Console.WriteLine(s2);
+            Console.WriteLine(s3);
+
+            Size size = new Size(640, 480);
+            Mat img = new Mat(size, MatType.CV_8UC3);
+            Mat img2 = new Mat(img.Size(), MatType.CV_8UC3);
+
+            Console.WriteLine($"{size.Width}, {size.Height}");
+            Console.WriteLine(img.Size());
+            Console.WriteLine($"{img.Size().Width}, {img.Size().Height}");
+            Console.WriteLine($"{img.Width}, {img.Height}");
+
+            Range range = new Range(0, 100);
+            Console.WriteLine($"{range.Start}, {range.End}");
+
+            Rect rect1 = new Rect(new Point(0, 0), new Size(640, 480));
+            Rect rect2 = new Rect(100, 100, 640, 480);
+            Rect rect3 = rect1.Union(rect2);
+            Rect rect4 = rect1.Intersect(rect2);
+            Console.WriteLine(rect1);
+            Console.WriteLine(rect2);
+            Console.WriteLine(rect3);
+            Console.WriteLine(rect4);
+
+
+        }
+    }
+}
